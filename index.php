e455dbacc225590f3c148b03b9e5c1f00d71d4a8
maxa <mando.maxa@gmail.com>
2019-07-09 19:32:15 +0200

    Alterei o primeiro echo e adicionei o Lembre-se, um dos pre-requisitos para fazer este curso e ter um conhecimento basico em HTML e CSS

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
 
-echo "Hello World!";
+echo "Hello World dos Desenvolvedores PHP!";
+
+echo "Lembre-se, um dos pre-requisitos para fazer este curso e ter um conhecimento basico em HTML e CSS.";
 
  ?>
